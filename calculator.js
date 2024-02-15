ea3571332afca07c32aad337474c3f2f5ddca768
mbrown4 <76119151+mbrown4@users.noreply.github.com>
2024-02-14 22:24:06 -0500

    Creating calculator.js

---
+++ calculator.js
@@ -0,0 +1,9 @@
+while (true) {
+    // Prompt for the first number
+    let x = parseFloat(prompt("Enter the first number:"));
+    if (isNaN(x)) {
+        alert("Invalid input. Please enter a valid number.");
+        continue;
+    }
+
+}
