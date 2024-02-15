cc7029f67c6f0cb72ba5fd23cc231b63ade379f3
mbrown4 <76119151+mbrown4@users.noreply.github.com>
2024-02-14 22:28:55 -0500

    Update calculator pop up

--- calculator.js
+++ calculator.js
@@ -1,9 +1,16 @@
 while (true) {
     // Prompt for the first number
-    let x = parseFloat(prompt("Enter the first number:"));
+    let x = parseFloat(prompt("Value of x"));
     if (isNaN(x)) {
         alert("Invalid input. Please enter a valid number.");
         continue;
     }
+
+    let y = parseFloat(prompt("Value of y"));
+    if (isNaN(y)) {
+        alert("Invalid input. Please enter a valid number.");
+        continue;
+    }
+
 
 }
