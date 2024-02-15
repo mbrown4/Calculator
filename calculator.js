ea41194e67debfff0e4e11a8a5cd2dedd338d4ed
mbrown4 <76119151+mbrown4@users.noreply.github.com>
2024-02-14 22:37:14 -0500

    Update calculator.js operators

--- calculator.js
+++ calculator.js
@@ -6,11 +6,33 @@ while (true) {
         continue;
     }
 
+    // Prompt for the second number
     let y = parseFloat(prompt("Value of y"));
     if (isNaN(y)) {
         alert("Invalid input. Please enter a valid number.");
         continue;
     }
 
+    // Prompt for the first number
+    let operator = prompt("Operator");
+    let result;
 
+    // Perform operator calculation
+    switch(operator){
+        case "+":
+            result = x + y;
+            break;
+        case "-":
+            result = x - y;
+            break;
+        case "*":
+            result = x * y;
+            break;
+        case "/":
+            result = x / y;
+            break;
+        case "%":
+            result = x % y;
+            break;
+    }
 }
